72cefcab31502f4bd49f4374ce23018a68b409d1
Guled Ahmed <akindnesstoremember@gmail.com>
2021-01-13 18:03:47 -0800

    I deleted a const variable that I didn't need

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Choices = require("inquirer/lib/objects/choices");
 
 const teamList = [];
 
